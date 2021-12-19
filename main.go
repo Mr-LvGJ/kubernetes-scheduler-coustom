f16dc7ac0d0d841b1806e395d251429e19729ef4
root <root@localhost.localdomain>
2021-12-19 23:29:23 +0800

    Add info code

--- main.go
+++ main.go
@@ -1,44 +1,39 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"math/rand"
-// 	"os"
-// 	"time"
-
-// 	"sample-scheduler-framework/pkg/plugins"
-
-// 	"k8s.io/component-base/logs"
-// 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
-// )
-
-// func main() {
-// 	rand.Seed(time.Now().UTC().UnixNano())
-
-// 	command := app.NewSchedulerCommand(
-// 		app.WithPlugin(plugins.Name, plugins.New),
-// 	)
-// 	logs.InitLogs()
-// 	defer logs.FlushLogs()
-
-// 	if err := command.Execute(); err != nil {
-// 		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
-// 		os.Exit(1)
-// 	}
-// }
-
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
 	"sample-scheduler-framework/pkg/info"
+	"sample-scheduler-framework/pkg/plugins"
+
+	"k8s.io/component-base/logs"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 )
 
+var i info.Result
+
 func main() {
-	var i info.Result
-	r, err := i.Init()
+	res, err := i.Init()
 	if err != nil {
 		return
 	}
-	for instance := range r.Info {
-		fmt.Println(instance, r.Info[instance].Cpu)
+	for nodeName := range res.Info {
+		fmt.Println(nodeName, res.Info[nodeName])
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(plugins.Name, plugins.New),
+	)
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 }
