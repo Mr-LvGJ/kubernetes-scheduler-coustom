23d96ff4a022f9f6bf108fca15808318f3adc6df
LvGJ <lvgj1998@qq.com>
2021-12-24 15:38:09 +0800

    Change yaml

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -3,13 +3,13 @@ package plugins
 import (
 	"context"
 	"fmt"
-	"k8s.io/klog"
+
+	"k8s.io/klog/v2"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	frameworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
-
 )
 
 const Name = "sample-plugin"
@@ -17,7 +17,7 @@ const Name = "sample-plugin"
 var (
 	_ framework.FilterPlugin    = &Sample{}
 	_ framework.PreFilterPlugin = &Sample{}
-	_ framework.PreBindPlugin = &Sample{}
+	_ framework.PreBindPlugin   = &Sample{}
 
 	scheme = runtime.NewScheme()
 )
@@ -33,14 +33,14 @@ type Sample struct {
 	handle framework.Handle
 }
 
-func New(rargs runtime.Object, h framework.Handle) (framework.Plugin, error)  {
+func New(rargs runtime.Object, h framework.Handle) (framework.Plugin, error) {
 	args := &Args{}
 	if err := frameworkruntime.DecodeInto(rargs, args); err != nil {
 		return nil, err
 	}
 	klog.V(3).Infof("get plugin config args: %+v", args)
 	return &Sample{
-		args: args,
+		args:   args,
 		handle: h,
 	}, nil
 }
@@ -58,7 +58,7 @@ func (s *Sample) Filter(ctx context.Context, state *framework.CycleState, pod *v
 func (s *Sample) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
 	if nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName); err != nil {
 		return framework.NewStatus(framework.Error, fmt.Sprintf("prebind get node info error: %+v", nodeName))
-	}else {
+	} else {
 		klog.V(3).Infof("prebind node info: %+v", nodeInfo.Node())
 		return framework.NewStatus(framework.Success, "")
 	}
@@ -72,5 +72,3 @@ func (s *Sample) PreFilterExtensions() framework.PreFilterExtensions {
 func (s *Sample) Name() string {
 	return Name
 }
-
-
